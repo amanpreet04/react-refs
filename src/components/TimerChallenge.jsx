348214d135d5f6eeb484668b32420fdba733cb24
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-10 00:14:08 +0530

    Used ref to clear timeout

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,37 @@
+import { useRef, useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const timeout = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timeout.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timeout.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
-      <p>
+      {timerExpired && <p>You lost!</p>}
+
+      <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button>Start Challenge</button>
-      <p>Time is running</p>
+
+      <button onClick={timerStarted ? handleStop : handleStart}>
+        {timerStarted ? "Stop" : "Start"} Challenge
+      </button>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running" : "Timer completed"}
+      </p>
     </section>
   );
 }
