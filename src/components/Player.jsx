96b3e4e9fafcfdf7be038e5d9a503620a70debfa
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-09 22:59:19 +0530

    Removed unnecessarycode

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,25 +3,15 @@ import { useRef, useState } from "react";
 export default function Player() {
   const playerNameRef = useRef();
   const [playerName, setPlayerName] = useState(null);
-  // const [submitted, setsubmitted] = useState(false);
-
-  // function handlePlayerNameChange(event) {
-  //   setPlayerName(event.target.value);
-  // }
 
   function handleSubmit() {
-    // setsubmitted(true);
     setPlayerName(playerNameRef.current.value);
   }
   return (
     <section id="player">
       <h2>Welcome {playerName ?? "unknown entity"} </h2>
       <p>
-        <input
-          ref={playerNameRef}
-          type="text"
-          // onChange={handlePlayerNameChange}
-        />
+        <input ref={playerNameRef} type="text" />
         <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
