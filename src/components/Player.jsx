7756d848111a0869cf856d096af233beba15c239
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-09 22:58:37 +0530

    Using refs instead of State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
-  const [submitted, setsubmitted] = useState(false);
+  const playerNameRef = useRef();
+  const [playerName, setPlayerName] = useState(null);
+  // const [submitted, setsubmitted] = useState(false);
 
-  function handlePlayerNameChange(event) {
-    setPlayerName(event.target.value);
-  }
+  // function handlePlayerNameChange(event) {
+  //   setPlayerName(event.target.value);
+  // }
 
   function handleSubmit() {
-    setsubmitted(true);
+    // setsubmitted(true);
+    setPlayerName(playerNameRef.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : "unknown entity"} </h2>
+      <h2>Welcome {playerName ?? "unknown entity"} </h2>
       <p>
-        <input type="text" onChange={handlePlayerNameChange} />
+        <input
+          ref={playerNameRef}
+          type="text"
+          // onChange={handlePlayerNameChange}
+        />
         <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
