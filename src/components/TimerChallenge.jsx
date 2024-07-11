a420b1442b802ad4de6f34a301f6ff61b8aff74f
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-11 23:54:40 +0530

    Used useImperativeHandle to decouple the function getting executed

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,7 +10,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timeout.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
